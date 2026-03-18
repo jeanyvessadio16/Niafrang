22a6ee489c91dc3c65d2306b26ebf8fa1f57c4f1
Jean Yves Sadio <jeanyvessadio7@gmail.com>
2026-03-18 00:31:36 +0000

    Nature

--- src/app/nature/page.tsx
+++ src/app/nature/page.tsx
@@ -1,16 +1,49 @@
 import HeaderTitle from "@/components/layout/HeaderTitle";
+// import { imageLinks } from "@/data/natureCulture";
+// import {
+//   Card,
+//   CardHeader,
+//   CardContent,
+//   CardTitle,
+//   CardDescription,
+// } from "@/components/ui/card";
+// import Image from "next/image";
 
 export default function Nature() {
-    return (
-        <>
-            {/* Header title */}
-            <div className="relative">
-                <HeaderTitle
-                    title="Nature et culture"
-                    description="Découvrez la beauté naturelle et la culture Diola"
-                    bgImage="/images/foyer.jpg"
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      {/* Header title */}
+      <div className="relative">
+        <HeaderTitle
+          title="Nature et culture"
+          description="Découvrez la beauté naturelle et la culture Diola"
+          bgImage="/images/foyer.jpg"
+        />
+      </div>
+
+      {/* galerie images */}
+      {/* <section>
+        <div className="p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+          {imageLinks.map((image) => (
+            <Card key={image.id}>
+              <CardHeader>
+                <CardTitle>
+                  <Image
+                    width={300}
+                    height={250}
+                    src={image.image}
+                    alt="image"
+                  />
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardTitle> {image.title} </CardTitle>
+                <CardDescription> {image.description} </CardDescription>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </section> */}
+    </>
+  );
 }
---
+++ src/data/natureCulture.ts
@@ -0,0 +1,20 @@
+export interface natureCulture {
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+}
+
+export const imageLinks: natureCulture[] = [
+  { id: 1, image: "/images/mangrove", title: "Mangrove", description: "" },
+  { id: 2, image: "/images/mangrove", title: "Riziere", description: "" },
+  { id: 3, image: "/images/mangrove", title: "Dune", description: "" },
+  { id: 4, image: "/images/mangrove", title: "Festival", description: "" },
+  {
+    id: 5,
+    image: "/images/mangrove",
+    title: "Danse diola karone",
+    description: "",
+  },
+  { id: 6, image: "/images/mangrove", title: "Hiteni", description: "" },
+];
